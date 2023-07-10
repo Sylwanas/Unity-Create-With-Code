82f9091a087f74324eca8c693a50c682fa7a28a8
Sylwanas <alice@harlid.se>
2023-07-10 12:54:10 +0200

    unit 2

---
+++ My project/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPreFab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPreFab, transform.position, dogPreFab.transform.rotation);
+        }
+    }
+}
